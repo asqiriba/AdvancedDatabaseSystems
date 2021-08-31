5ce78eb115be606f82935188eb70985993b65ed4
asqiriba <hassan@asqiriba.com>
2021-08-31 11:38:42 -0700

    add solutions into ch2hw

--- Chapter2 Structured Query Language/Chapter 2 Homework.sql
+++ Chapter2 Structured Query Language/Chapter 2 Homework.sql
@@ -5,6 +5,8 @@
     (link to a google search, youtubevideo, class notes...)
 */
 
+USE CHAPTER2HW;
+
 -- 1.Show all data in each of the tables(5points)
 SELECT * FROM CUSTOMER, SALE, SALE_ITEM, ITEM;
 
@@ -28,21 +30,38 @@ SELECT LASTNAME, FIRSTNAME, PHONE FROM CUSTOMER WHERE PHONE LIKE "%56_";
 SELECT MAX(TOTAL) AS 'MAX', MIN(TOTAL) AS 'MIN', AVG(TOTAL) AS 'AVG' FROM SALE;
 
 -- 8.Count the number of customers having each combination of LastName and FirstName(5 points)
-SELECT COUNT(*) FROM CUSTOMER GROUP BY LASTNAME, FIRSTNAME HAVING COUNT(*) > 1;
+SELECT LASTNAME, FIRSTNAME, COUNT(*) AS 'No. Combinations' FROM CUSTOMER GROUP BY LASTNAME, FIRSTNAME;
 
 -- 9.Show the LastName, FirstName, and Phone of all customers who have had an order with Total  greater  than  $100.00.  Use  a  subquery. 
 -- Present  the  results  sorted  by LastName  in ascending order and then FirstName in descending order.(10points)
 SELECT LASTNAME, FIRSTNAME, PHONE
-FROM CUSTOMER C, SALE S 
+FROM CUSTOMER C
 WHERE C.CUSTOMERID IN (SELECT S.CUSTOMERID FROM SALE S WHERE S.TOTAL > 100 AND C.CUSTOMERID = S.CUSTOMERID)
-GROUP BY LASTNAME ASC
-ORDER BY FIRSTNAME DESC;
+GROUP BY LASTNAME ASC, FIRSTNAME DESC;
 
--- 10.Show the LastName, FirstName, and Phone of all customers who have had an order with Total greater than $100.00. Use a join. 
+-- 10.Show the LastName, FirstName, and Phone of all customers who have had an order with Total greater than $100.00. Use a join.
 -- Present the results sorted by LastName in ascending order and then FirstName in descending order.(10points)
+SELECT C.LASTNAME, C.FIRSTNAME, C.PHONE
+FROM CUSTOMER C JOIN SALE S
+ON C.CUSTOMERID = S.CUSTOMERID
+WHERE S.TOTAL > 100
+GROUP BY LASTNAME ASC, FIRSTNAME DESC;
 
 -- 11.Show  the  LastName,  FirstName,  and  Phone  of  all  customers  who  have  bought  an  Item named ‘Desk Lamp’.
 -- Use a subquery, Present results sorted by LastName in ascending order and then FirstName is descending order.(20points)
+SELECT C.LASTNAME, C.FIRSTNAME, C.PHONE
+FROM CUSTOMER C
+WHERE C.CUSTOMERID IN (
+SELECT S.CUSTOMERID
+FROM SALE S 
+WHERE S.SALEID IN (
+	SELECT SI.SALEID
+    FROM SALE_ITEM SI, ITEM I
+    WHERE SI.SALEITEMID = I.ITEMID
+    AND I.ITEMDESCRIPTION LIKE 'DESK LAMP'
+	)
+)
+GROUP BY LASTNAME ASC, FIRSTNAME DESC;
 
 -- 12.Show  the  LastName,  FirstName,  and  Phone  of  all  customers  who  have  bought  an  Item named ‘Desk Lamp’.
 -- Use a join, Present results sorted by LastName in ascending order and then FirstName is descending order.(20points)
